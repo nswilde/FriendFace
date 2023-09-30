926c4ce9c33b860da88c2a7111fb40ed8b460f5e
opheliacodes <142635621+opheliacodes@users.noreply.github.com>
2023-10-01 11:03:09 +1300

    Initial Commit

---
+++ FriendFacenew/FriendFacenewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFacenewApp.swift
+//  FriendFacenew
+//
+//  Created by Nikki Wilde on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFacenewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
